88d96f66eaf01ce632bf61c2d81801c25e785a2d
partofship <strongandrew@naver.com>
2026-01-15 14:33:04 +0900

    :sparkles: feat(#5): 유저 모델 작성

---
+++ apps/accounts/models/user.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models
+
+from apps.accounts.models.user_enumerate import (
+    GenderChoices,
+    UserRoleChoices,
+    UserStatus,
+)
+from apps.accounts.models.user_timestamp import UserTimeStampedModel
+
+
+class User(AbstractBaseUser, UserTimeStampedModel):
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=10)
+    gender = models.CharField(choices=GenderChoices.choices, max_length=1)
+    phone = models.CharField(max_length=15)
+    birthday = models.DateField()
+    profile_image_url = models.CharField(max_length=255)  # URLField?
+    status = models.CharField(choices=UserStatus.choices, max_length=12)
+    role = models.CharField(
+        choices=UserRoleChoices.choices, max_length=20, default=UserRoleChoices.USER
+    )
