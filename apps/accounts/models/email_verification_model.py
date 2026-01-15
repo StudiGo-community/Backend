b050316bfd729b2be01dce12dc241077aa187979
partofship <strongandrew@naver.com>
2026-01-15 18:11:52 +0900

    :sparkles: feat(#10): 이메일, 휴대전화 인증 테이블 작성

---
+++ apps/accounts/models/email_verification_model.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from apps.core.models import VerificationTimeStampedModel
+from apps.accounts.models.user_enumerate import VerificationPurpose
+
+class EmailVerification(VerificationTimeStampedModel):
+    email = models.CharField(max_length=30)
+    code = models.CharField(max_length=255)
+    purpose = models.CharField(choices=VerificationPurpose.choices, default=VerificationPurpose.SIGNUP)
+
+    class Meta:
+        db_table = "email_verification"
