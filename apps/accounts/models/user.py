e5002a7eff6270c3056097fe1169e6268e966d5f
partofship <strongandrew@naver.com>
2026-01-15 17:52:46 +0900

    :sparkles: feat(#10): 소셜로그인계정 모델 추가

--- apps/accounts/models/user.py
+++ apps/accounts/models/user.py
@@ -5,19 +5,44 @@ from apps.accounts.models.user_enumerate import (
     GenderChoices,
     UserRoleChoices,
     UserStatus,
+    SocialProviderChoices,
 )
 from apps.core.models import TimeStampedModel
 
 
 class User(AbstractBaseUser, TimeStampedModel):
-    email = models.EmailField(unique=True)
+    email = models.CharField(max_length=30, unique=True) # 50~100 정도로 수정?
     nickname = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=10)
     gender = models.CharField(choices=GenderChoices.choices, max_length=1)
     phone = models.CharField(max_length=15)
-    birthday = models.DateField()
-    profile_image_url = models.CharField(max_length=255)  # URLField?
-    status = models.CharField(choices=UserStatus.choices, max_length=12)
+    birthday = models.DateField(null=True, blank=True)
+    profile_image_url = models.CharField(max_length=255, null=True, blank=True)  # URLField?
+    status = models.CharField(choices=UserStatus.choices, max_length=12, default=UserStatus.ACTIVE)
     role = models.CharField(
         choices=UserRoleChoices.choices, max_length=20, default=UserRoleChoices.USER
     )
+
+    class Meta:
+        db_table = "user"
+
+class OAuthAccount(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    provider = models.CharField(choices=SocialProviderChoices.choices, max_length=12)
+    provider_user_id = models.CharField(max_length=255)
+    social_email = models.CharField(max_length=255)
+    access_token = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "oauth_account"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["provider", "provider_user_id"],
+                name="unique_oauth_provider_provider_user_id",
+            )
+        ]
+        indexes = [
+            models.Index(fields=["user"], name="idx_oauth_user"),
+            models.Index(fields=["provider"], name="idx_oauth_provider"),
+        ]
