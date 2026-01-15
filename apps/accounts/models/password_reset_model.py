d1701dc06f03111e5327bee2aac39e307d06a46c
partofship <strongandrew@naver.com>
2026-01-15 18:41:54 +0900

    :bulb: chore(#10): 토큰 관련 내역은 redis로 관리할 예정

--- apps/accounts/models/password_reset_model.py
+++ apps/accounts/models/password_reset_model.py
@@ -5,7 +5,6 @@ from apps.accounts.models.user import User
 
 class PasswordResetRequest(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    token = models.CharField(max_length=255)
     expires_at = models.DateTimeField()
     used_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
