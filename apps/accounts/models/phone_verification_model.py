33d6bf599e9bc6777fc6ece4218a90695caa9706
partofship <strongandrew@naver.com>
2026-01-16 17:26:25 +0900

    :bulb: chore(#24): 이전 PR 내역 반영

--- apps/accounts/models/phone_verification_model.py
+++ apps/accounts/models/phone_verification_model.py
@@ -1,16 +1,13 @@
 from django.db import models
 
-from apps.core.enumeration.account_verification_enumeration import VerificationPurpose
+from apps.core.enumeration.account_verification_enumeration import PhoneVerificationPurpose
 from apps.core.models import VerificationTimeStampedModel
 
 
 class PhoneVerification(VerificationTimeStampedModel):
     phone = models.CharField(max_length=30)
     code = models.CharField(max_length=255)
-    purpose = models.CharField(
-        choices=VerificationPurpose.choices,
-        default=VerificationPurpose.PHONE_VERIFICATION,
-    )
+    purpose = models.CharField(choices=PhoneVerificationPurpose.choices)
 
     class Meta:
         db_table = "phone_verifications"
