90ee903e507f4201fb16d9ecb9e1f01ec3c8c745
fullrestic <restinplastic@naver.com>
2026-01-19 16:43:14 +0900

    ✨ feat     : Swagger 설정 추가 & settings 정리

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import URLPattern, URLResolver, include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
-urlpatterns = [
+urlpatterns: list[URLPattern | URLResolver] = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("apps.community.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    if "debug_toolbar" in settings.INSTALLED_APPS:
+        urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
+    if "drf_spectacular" in settings.INSTALLED_APPS:
+        urlpatterns += [
+            path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+            path(
+                "api/schema/swagger-ui/",
+                SpectacularSwaggerView.as_view(url_name="schema"),
+                name="swagger-ui",
+            ),
+            path(
+                "api/schema/redoc/",
+                SpectacularRedocView.as_view(url_name="schema"),
+                name="redoc",
+            ),
+        ]
