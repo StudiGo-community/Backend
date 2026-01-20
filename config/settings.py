91f68a142f5bc2d46aeb068d7f20c4327103c3c9
fullrestic <restinplastic@naver.com>
2026-01-20 17:10:04 +0900

    ✨ feat     : 세션 인증 추가

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
