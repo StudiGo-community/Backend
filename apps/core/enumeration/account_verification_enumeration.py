6cd91295153593bb3895f7e54a5bfa18968fbb04
partofship <strongandrew@naver.com>
2026-01-22 08:51:04 +0900

    :bulb: chore(#24): isort, black format applied

--- apps/core/enumeration/account_verification_enumeration.py
+++ apps/core/enumeration/account_verification_enumeration.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 class EmailVerificationPurpose(models.TextChoices):
     SIGNUP = "SIGNUP"
     PASSWORD_RESET = "PASSWORD_RESET"
     # EMAIL_CHANGE = "EMAIL_CHANGE"
+
 
 class PhoneVerificationPurpose(models.TextChoices):
     SIGNUP = "SIGNUP"
