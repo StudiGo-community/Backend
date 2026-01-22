3680681a0e65a5131c3ee2b86a53f185bdb8163e
1yul2 <hanyul9824@gmail.com>
2026-01-22 18:58:03 +0900

    ✨feat : 좋아요,취소 urls

--- apps/community/urls/post_urls.py
+++ apps/community/urls/post_urls.py
@@ -1,8 +1,13 @@
 from django.urls import URLPattern, URLResolver, path
 
-from apps.community.views.post_views import PostCreateListAPIView, PostDetailAPIView
+from apps.community.views.post_views import (
+    PostCreateListAPIView,
+    PostDetailAPIView,
+    PostLikeView,
+)
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path("posts/", PostCreateListAPIView.as_view(), name="posts"),
     path("posts/<int:post_id>", PostDetailAPIView.as_view(), name="post_detail"),
+    path("posts/<int:post_id>/like/", PostLikeView.as_view(), name="post_like"),
 ]
