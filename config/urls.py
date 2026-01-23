fd6cf3d130d558f03c6b4e92a8783edc1e17f0b9
김민주 <minjuju518@naver.com>
2026-01-23 14:28:58 +0900

    ✨ feat: chat url 추가

--- config/urls.py
+++ config/urls.py
@@ -29,6 +29,8 @@ urlpatterns: list[URLPattern | URLResolver] = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("apps.accounts.urls")),
     path("api/v1/", include("apps.community.urls")),
+    path("api/v1/chat/", include("apps.chat.api.urls")),
+
 ]
 
 if settings.DEBUG:
