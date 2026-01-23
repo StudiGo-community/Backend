13d66765b5ab444352cef4c91c5c1b4bf1eb6c65
1yul2 <hanyul9824@gmail.com>
2026-01-23 16:19:24 +0900

    ✨feat : 댓글 목록 조회 응답 시리얼라이저 추가

--- apps/community/serializers/comment_serializers.py
+++ apps/community/serializers/comment_serializers.py
@@ -1,13 +1,10 @@
 import re
+from typing import Any, TypedDict
 
-from django.db import transaction
-from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
 from apps.community.models.comments import Comment
-from apps.community.models.posts import Post
 from apps.community.serializers.common_serializers import AuthorSerializer
-from apps.core.enumeration.community_enumerations import PostCommentStatus
 
 MENTION_REGEX = re.compile(r"@([A-Za-z0-9_.가-힣]{1,30})")
 
@@ -43,3 +40,29 @@ class CommentResponseSerializer(serializers.ModelSerializer[Comment]):
 
 class CommentCreateSerializer(serializers.Serializer[Comment]):
     content = serializers.CharField()
+
+
+class CommentListItemSerializer(serializers.ModelSerializer[Comment]):
+    author = AuthorSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = (
+            "id",
+            "author",
+            "content",
+            "created_at",
+        )
+
+
+class PaginationDict(TypedDict):
+    page: int
+    size: int
+    total_count: int
+    total_pages: int
+    has_next: bool
+
+
+class CommentListResponseSerializer(serializers.Serializer[dict[str, Any]]):
+    comments = CommentListItemSerializer(many=True)
+    pagination = serializers.DictField()
