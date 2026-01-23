c4440752bf3652631574a1b2b1b8c133d4a568b9
김민주 <minjuju518@naver.com>
2026-01-23 19:01:35 +0900

    🎨 style: ci야 터지지 말아라

--- apps/chat/api/urls.py
+++ apps/chat/api/urls.py
@@ -5,6 +5,8 @@ from apps.chat.views.room_view import RoomDetailAPIView, RoomListAPIView
 urlpatterns = [
     path("chat/rooms/", RoomListAPIView.as_view(), name="chat-room-list-create"),
     path(
-        "chat/rooms/<int:room_id>/", RoomDetailAPIView.as_view(), name="chat-room-detail"
+        "chat/rooms/<int:room_id>/",
+        RoomDetailAPIView.as_view(),
+        name="chat-room-detail",
     ),
 ]
--- apps/chat/views/room_view.py
+++ apps/chat/views/room_view.py
@@ -18,20 +18,14 @@ from apps.chat.services.room_service import create_room
 
 
 class RoomListAPIView(APIView):
-    @extend_schema(
-        summary="채팅방 목록 조회",
-        tags=["chat"]
-    )
+    @extend_schema(summary="채팅방 목록 조회", tags=["chat"])
     def get(self, request: Request) -> Response:
         rooms = get_rooms()
         return Response(
             RoomSerializer(rooms, many=True).data, status=status.HTTP_200_OK
         )
 
-    @extend_schema(
-        summary="채팅방 목록 생성",
-        tags=["chat"]
-    )
+    @extend_schema(summary="채팅방 목록 생성", tags=["chat"])
     def post(self, request: Request) -> Response:
         serializer = RoomCreateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -42,11 +36,9 @@ class RoomListAPIView(APIView):
         )
         return Response(RoomDetailSerializer(room).data, status=status.HTTP_201_CREATED)
 
+
 class RoomDetailAPIView(APIView):
-    @extend_schema(
-        summary="채팅방 디테일",
-        tags=["chat"]
-    )
+    @extend_schema(summary="채팅방 디테일", tags=["chat"])
     def get(self, request: Request, room_id: int) -> Response:
         room = get_object_or_404(Room, pk=room_id)
         return Response(RoomDetailSerializer(room).data, status=status.HTTP_200_OK)
