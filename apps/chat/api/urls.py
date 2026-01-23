611631f93df6404224fc9566ea2b1487cc4d6d0b
김민주 <minjuju518@naver.com>
2026-01-23 18:39:16 +0900

    🐛 fix: chat url 구조 수정

--- apps/chat/api/urls.py
+++ apps/chat/api/urls.py
@@ -4,5 +4,7 @@ from apps.chat.views.room_view import RoomDetailAPIView, RoomListAPIView
 
 urlpatterns = [
     path("chat/rooms", RoomListAPIView.as_view(), name="chat-room-list-create"),
-    path("chat/rooms/<int:room_id>", RoomDetailAPIView.as_view(), name="chat-room-detail"),
+    path(
+        "chat/rooms/<int:room_id>", RoomDetailAPIView.as_view(), name="chat-room-detail"
+    ),
 ]
