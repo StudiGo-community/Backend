50c000c1aefb78995b6f19f2ffe8c181040e69e0
김민주 <minjuju518@naver.com>
2026-01-23 18:57:24 +0900

    🐛 fix: chat 스웨거 추가

--- apps/chat/views/room_view.py
+++ apps/chat/views/room_view.py
@@ -1,6 +1,7 @@
 # 방 목록, 생성, 삭제
 
 from django.shortcuts import get_object_or_404, render
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -17,12 +18,20 @@ from apps.chat.services.room_service import create_room
 
 
 class RoomListAPIView(APIView):
+    @extend_schema(
+        summary="채팅방 목록 조회",
+        tags=["chat"]
+    )
     def get(self, request: Request) -> Response:
         rooms = get_rooms()
         return Response(
             RoomSerializer(rooms, many=True).data, status=status.HTTP_200_OK
         )
 
+    @extend_schema(
+        summary="채팅방 목록 생성",
+        tags=["chat"]
+    )
     def post(self, request: Request) -> Response:
         serializer = RoomCreateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -33,8 +42,11 @@ class RoomListAPIView(APIView):
         )
         return Response(RoomDetailSerializer(room).data, status=status.HTTP_201_CREATED)
 
-
 class RoomDetailAPIView(APIView):
+    @extend_schema(
+        summary="채팅방 디테일",
+        tags=["chat"]
+    )
     def get(self, request: Request, room_id: int) -> Response:
         room = get_object_or_404(Room, pk=room_id)
         return Response(RoomDetailSerializer(room).data, status=status.HTTP_200_OK)
