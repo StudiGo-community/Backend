60d068847517881118e83a96c7d96f14a12cc566
김민주 <minjuju518@naver.com>
2026-01-23 18:31:23 +0900

    🐛 fix: mypy 터져서 수정

---
+++ apps/chat/models/__init__.py
@@ -0,0 +1,7 @@
+from .bans import Bans
+from .membership import Membership
+from .message import Message
+from .room import Room
+from .translation import Translation
+
+__all__ = ["Room", "Membership", "Message", "Translation", "Bans"]
