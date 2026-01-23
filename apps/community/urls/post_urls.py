bada9e3ee4f8d179dad32656fe8c71a84dcd06af
1yul2 <hanyul9824@gmail.com>
2026-01-23 10:39:39 +0900

    ♻️refactor : 마지막 슬래시 제거

--- apps/community/urls/post_urls.py
+++ apps/community/urls/post_urls.py
@@ -7,7 +7,7 @@ from apps.community.views.post_views import (
 )
 
 urlpatterns: list[URLPattern | URLResolver] = [
-    path("posts/", PostCreateListAPIView.as_view(), name="posts"),
+    path("posts", PostCreateListAPIView.as_view(), name="posts"),
     path("posts/<int:post_id>", PostDetailAPIView.as_view(), name="post_detail"),
-    path("posts/<int:post_id>/like/", PostLikeView.as_view(), name="post_like"),
+    path("posts/<int:post_id>/like", PostLikeView.as_view(), name="post_like"),
 ]
