03c067a25899b709d011c79af273ff3954747391
fullrestic <restinplastic@naver.com>
2026-01-23 15:44:10 +0900

    ✨ feat     : 토큰 갱신 & 로그아웃 API url 등록

--- apps/accounts/urls/auth_urls.py
+++ apps/accounts/urls/auth_urls.py
@@ -1,7 +1,9 @@
 from django.urls import URLPattern, URLResolver, path
 
-from apps.accounts.views.auth_views import LoginView
+from apps.accounts.views.auth_views import LoginView, LogoutAPIView, TokenRefreshAPIView
 
 urlpatterns: list[URLPattern | URLResolver] = [
-    path("auth/login", LoginView.as_view(), name="email_login"),
+    path("auth/login", LoginView.as_view(), name="auth_login"),
+    path("auth/refresh", TokenRefreshAPIView.as_view(), name="auth_refresh"),
+    path("auth/logout", LogoutAPIView.as_view(), name="auth_logout"),
 ]
