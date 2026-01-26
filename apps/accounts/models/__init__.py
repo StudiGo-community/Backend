86eb842738d41f1d92981293c7b619f9a43fceb6
partofship <strongandrew@naver.com>
2026-01-26 15:01:21 +0900

    :bulb:  chore(#24): verification 모델 통합 반영

--- apps/accounts/models/__init__.py
+++ apps/accounts/models/__init__.py
@@ -1,17 +1,14 @@
 # 외부에서 account.models.User 형태로 사용하기 위한 import
 from apps.accounts.models.attendance_record_model import AttendanceRecord
-from apps.accounts.models.email_verification_model import EmailVerification
+from apps.accounts.models.verification_model import Verification
 from apps.accounts.models.password_reset_model import PasswordResetRequest
-from apps.accounts.models.phone_verification_model import PhoneVerification
 from apps.accounts.models.users import OAuthAccount, User, UserManager
 
 __all__ = [
     # attendance_record
     "AttendanceRecord",
-    # 이메일인증
-    "EmailVerification",
-    # 핸드폰인증
-    "PhoneVerification",
+    # 인증
+    "Verification",
     # 비밀번호 재설정 요청
     "PasswordResetRequest",
     # 유저
