6ef5febcdc2d0fcb83dcaf780f2fe29c73caa29c
fullrestic <restinplastic@naver.com>
2026-01-26 18:17:56 +0900

    🎨 style    : 코드 포매팅

--- apps/core/enumeration/account_verification_enumeration.py
+++ apps/core/enumeration/account_verification_enumeration.py
@@ -5,6 +5,7 @@ class EmailVerificationPurpose(models.TextChoices):
     SIGNUP = "SIGNUP", "회원가입"
     PASSWORD_RESET = "PASSWORD_RESET", "비밀번호 재설정"
 
+
 class PhoneVerificationPurpose(models.TextChoices):
     SIGNUP = "SIGNUP", "회원가입"
     FIND_EMAIL = "FIND_EMAIL", "이메일 찾기"
