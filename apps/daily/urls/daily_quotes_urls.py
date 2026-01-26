aa7dededc6f0f83f445b7937d44350c48f47f4ee
1yul2 <hanyul9824@gmail.com>
2026-01-26 20:04:59 +0900

    ✨feat:  API URL 라우팅 추가

---
+++ apps/daily/urls/daily_quotes_urls.py
@@ -0,0 +1,10 @@
+from django.urls import URLPattern, URLResolver, path
+
+from apps.daily.views.daily_quotes_views import DailyQuoteView
+
+urlpatterns: list[URLPattern | URLResolver] = [
+    path("daily-quotes", DailyQuoteView.as_view()),
+
+]
+
+
