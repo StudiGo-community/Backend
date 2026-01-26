f800f785ad7abb8e8d5db07ea427d6476fb18ace
partofship <strongandrew@naver.com>
2026-01-26 10:56:34 +0900

    :bulb: chore(#24): migration 위해 수정
    # Conflicts:
    #	apps/accounts/models/__init__.py

--- apps/accounts/models/__init__.py
+++ apps/accounts/models/__init__.py
@@ -1,4 +1,21 @@
 # 외부에서 account.models.User 형태로 사용하기 위한 import
-from .users import OAuthAccount, User
+from apps.accounts.models.attendance_record_model import AttendanceRecord
+from apps.accounts.models.email_verification_model import EmailVerification
+from apps.accounts.models.password_reset_model import PasswordResetRequest
+from apps.accounts.models.phone_verification_model import PhoneVerification
+from apps.accounts.models.users import OAuthAccount, User, UserManager
 
-__all__ = ["User", "OAuthAccount"]
+__all__ = [
+    # attendance_record
+    "AttendanceRecord",
+    # 이메일인증
+    "EmailVerification",
+    # 핸드폰인증
+    "PhoneVerification",
+    # 비밀번호 재설정 요청
+    "PasswordResetRequest",
+    # 유저
+    "User",
+    "UserManager",
+    "OAuthAccount",
+]
