2d36a164b6d7b8ef217aa1d3ffcd6d44a3d1830a
fullrestic <restinplastic@naver.com>
2026-01-29 22:46:37 +0900

    ✨ feat     : exception handler 등록

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,7 @@ REST_FRAMEWORK = {
         "apps.accounts.authentication.CookieOrHeaderJWTAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "EXCEPTION_HANDLER": "apps.core.exceptions.custom_exception_handler",
 }
 
 REDIS_HOST = os.getenv("REDIS_HOST")
