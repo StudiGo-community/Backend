f5e624d0b73ed41d99193cf9de6d7f6e646c77b9
김민주 <minjuju518@naver.com>
2026-01-30 11:40:12 +0900

    🐛 fix: 스페인어 > 한국어 번역 추가

--- apps/core/translation.py
+++ apps/core/translation.py
@@ -13,3 +13,15 @@ def translate_ko_to_es(text: str) -> str:
     except Exception as e:
         print(e)  # 실패케이스 임시처리
         return text
+
+
+def translate_es_to_ko(text: str) -> str:
+    """
+    스페인어ES > 한글KO 번역
+    """
+    try:
+        result = translator.translate(text, src="es", dest="ko")
+        return str(result.text)
+    except Exception as e:
+        print(e)  # 실패케이스 임시처리
+        return text
