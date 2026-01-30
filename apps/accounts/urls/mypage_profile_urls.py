b478503c79f947d96d2eba9414fbef1793f7b6e6
partofship <strongandrew@naver.com>
2026-01-31 00:12:13 +0900

    :bulb: chore(#41): urls 타입힌트 추가

--- apps/accounts/urls/mypage_profile_urls.py
+++ apps/accounts/urls/mypage_profile_urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import URLPattern, URLResolver, path
 
 from apps.accounts.views.mypage_profile_views import (
     PasswordChangeView,
@@ -6,18 +6,18 @@ from apps.accounts.views.mypage_profile_views import (
     ProfileView,
 )
 
-urlpatterns = [
+urlpatterns: list[URLPattern | URLResolver] = [
     # 프로필 조회 및 수정
-    path("api/v1/users/me/profile", ProfileView.as_view(), name="profile"),
+    path("me/profile", ProfileView.as_view(), name="profile"),
     # 프로필 이미지 업로드
     path(
-        "api/v1/users/me/profile/image",
+        "me/profile/image",
         ProfileImageView.as_view(),
         name="profile-image",
     ),
     # 비밀번호 변경
     path(
-        "api/v1/users/me/profile/password",
+        "me/profile/password",
         PasswordChangeView.as_view(),
         name="password-change",
     ),
