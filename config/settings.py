6e0eabf7399558984d08291d42e9004e3d43f840
fullrestic <restinplastic@naver.com>
2026-02-05 15:19:59 +0900

    🐛 fix      : debug toolbar 제거

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,6 @@ THIRD_PARTY_APPS = [
     "channels",
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
-    "debug_toolbar",
 ]
 
 LOCAL_APPS = [
@@ -50,7 +49,6 @@ LOCAL_APPS = [
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
@@ -243,10 +241,6 @@ CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:3000"
 CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
 
 CORS_ALLOW_CREDENTIALS = True
-
-# debug_toolbar 표시용
-INTERNAL_IPS = ["127.0.0.1", "localhost", "0.0.0.0"]
-INTERNAL_IPS += ["172.17.0.1", "172.18.0.1"]
 
 # AWS
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "")
--- config/urls.py
+++ config/urls.py
@@ -36,8 +36,6 @@ urlpatterns: list[URLPattern | URLResolver] = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    if "debug_toolbar" in settings.INSTALLED_APPS:
-        urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
     if "drf_spectacular" in settings.INSTALLED_APPS:
         urlpatterns += [
             path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
