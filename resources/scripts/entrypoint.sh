dd07f8e28a95c70e1ac33f73774f04d6ba9eabf9
moxopills <pills87@kakao.com>
2026-02-06 15:56:33 +0900

    🐛 fix      : dephne -> uvicorn 변경

--- resources/scripts/entrypoint.sh
+++ resources/scripts/entrypoint.sh
@@ -9,5 +9,5 @@ echo "2. Database Migration 수행..."
 poetry run python manage.py migrate
 echo ""
 
-echo "3. Daphne(ASGI) 서버 실행..."
-exec poetry run daphne -b 0.0.0.0 -p 8000 config.asgi:application
+echo "3. Uvicorn(ASGI) 서버 실행..."
+exec poetry run uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --workers 3
