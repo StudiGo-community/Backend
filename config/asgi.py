6bb295cd1dd73eab6ccb27df17c739a95ceb8c02
1yul2 <hanyul9824@gmail.com>
2026-02-06 11:05:08 +0900

    🐛fix : asgi application

--- config/asgi.py
+++ config/asgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 from apps.chat.websocket.middlewares import JwtAuthMiddlewareStack
@@ -21,7 +22,11 @@ application = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
-        "http": application,
-        "websocket": JwtAuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            JwtAuthMiddlewareStack(
+                URLRouter(websocket_urlpatterns)
+            )
+        ),
     }
 )
