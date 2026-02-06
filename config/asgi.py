56dcde88be9813d35e33fa502524b77f9dc0ef46
moxopills <pills87@kakao.com>
2026-02-06 16:52:06 +0900

    🐛 fix      : 찐찐찐 막

--- config/asgi.py
+++ config/asgi.py
@@ -9,16 +9,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+# Django 설정 초기화 먼저
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
+# Django 초기화 후 channels import
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
 
 from apps.chat.websocket.middlewares import JwtAuthMiddlewareStack
 from apps.chat.websocket.routing import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
